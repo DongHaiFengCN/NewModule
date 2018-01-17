d9982b594e59abc616a245173a7a94d42082f6fa
DOAING <hf.dong@outlook.com>
2018-01-17 16:57:27 +0800

    第一次修改

--- order/src/main/java/doaing/order/Main2Activity.java
+++ order/src/main/java/doaing/order/Main2Activity.java
@@ -5,9 +5,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.tencent.bugly.crashreport.CrashReport;
 
 @Route(path = "/test/activity")
 public class Main2Activity extends AppCompatActivity {
@@ -23,8 +25,11 @@ public class Main2Activity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                String name = null;
+                Log.e("DOAING",name);
+                //CrashReport.testJavaCrash();
+               /* Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
             }
         });
     }
--- order/src/main/java/doaing/order/MainActivity.java
+++ order/src/main/java/doaing/order/MainActivity.java
@@ -2,10 +2,12 @@ package doaing.order;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
 
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.jakewharton.rxbinding.view.RxView;
+import com.tencent.bugly.crashreport.CrashReport;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,10 +29,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
         RxView.clicks(bt).subscribe(new Action1<Void>() {
             @Override
             public void call(Void aVoid) {
-                ARouter.getInstance().build("/test/activity").navigation();
+            ARouter.getInstance().build("/test/activity").navigation();
+
 
 
             }
